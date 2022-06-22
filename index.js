52564aac4a5e488ed4074cc01503bd0a51b635b7
Ofeghena <aosigbemhe2015@gmail.com>
2022-06-22 15:01:38 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let Name = "Osigbemhe Ofe Augustine";
+let Height = "181cm";
+let Country = "Nigeria";
+console.log("Name: " + Name);
+console.log("Height: " + Height);
+console.log("Country: " + Country);
